c238116c08960417b1474d815695e466b1cf2b7d
kabs muuka <kabwalimuuka@gmail.com>
2024-05-25 10:20:39 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+#Weather App
+
+It Dynamically fetches weather data of any specified City in the World.
+
+When a User enters a preffered City, the backend gets a weather api and append a city name at the end. If City Name is valid, It goes ahead and send that data back to the User or Client.
+
+NOTE BEFORE DOING ANYTHNG
+Copy paste the following in Mysql commandline
+
+CREATE DATABASE weatherApp;
+USE weatherApp;
+
+CREATE TABLE cities(
+id INT AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(256) NOT NULL,
+region VARCHAR(256) NOT NULL,
+country VARCHAR(256) NOT NULL,
+`localtime` DATETIME NOT NULL,
+temp_C DECIMAL(5, 2) NOT NULL,
+`text` VARCHAR(256) NOT NULL  
+);
+
+Then
+cd into backend
+1.npm install
+2.npm run start //http://localhost:3000
+
+Then
+cd into frontend
+run index.html
---
+++ backend/server.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const app = express();
+const connection = require("./db/database");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const port = 3000;
+
+//middleware
+app.use(cors());
+app.use(bodyParser.json());
+//end of middleware
+
+app.post("/api/weather", async (req, res) => {
+  const { city } = req.body;
+  if (!city) {
+    return res
+      .status(401)
+      .json({ error: "use propper City Names E.g. lusaka" });
+  }
+  try {
+    const response = await fetch(
+      `https://api.weatherapi.com/v1/current.json?key=42e6202c53c94387b0d82647232012&q=${city}`
+    );
+    const weatherdata = await response.json();
+    res.status(200).json(weatherdata); //send to the frontend
+
+    //destracturing weather data in order to save in properly into the database
+    const { name, region, country, localtime } = weatherdata.location;
+    const { temp_c } = weatherdata.current;
+    const { text } = weatherdata.current.condition;
+
+    connection
+      .promise()
+      .query(
+        "INSERT INTO cities(name, region, country, `localtime`, temp_c, `text`) VALUES(?, ?, ?, ?, ?, ?)",
+        [name, region, country, localtime, temp_c, text]
+      )
+      .then((result) => {
+        if (!result) {
+          console.log(
+            "failed to save data into the database! inspect database tables"
+          );
+        } else {
+          console.log("successfully saved your data ");
+        }
+      });
+  } catch (error) {
+    res
+      .status(404)
+      .json({ error: `Specified city name : ${city} Not found` }, error);
+  }
+});
+
+//getting data from the database
+app.get("/storedData", (req, res) => {
+  connection.query(`SELECT * FROM cities`, (error, result) => {
+    if (error) throw error;
+    res.status(200).json(result);
+  });
+});
+
+app.listen(port, () => {
+  connection.connect((err) => {
+    if (err) {
+      console.error("Error connecting to the database:", err.stack);
+      return;
+    }
+    console.log("Connected to the database as id " + connection.threadId);
+  });
+  console.log("server running on port " + port);
+});
